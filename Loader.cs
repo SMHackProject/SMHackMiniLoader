1216c2e47dd11a6a79b04935f479e58c1858c400
CodeHz <CodeHz@Outlook.com>
2017-08-25 18:09:26 +0800

    [Feature] Plugin parameters
    Signed-off-by: CodeHz <CodeHz@Outlook.com>

--- Loader.cs
+++ Loader.cs
@@ -2,15 +2,23 @@
     using System;
     using System.Collections.Concurrent;
     using System.Diagnostics;
+    using System.IO;
     using SMHackCore;
 
     public class Loader : ServerInterface {
-        public override string PluginConfigPath { get; }
         private readonly BlockingCollection<int> _pids = new BlockingCollection<int>();
-        
-        public override void Connect(int id, string image) {  }
 
-        public override void DoLog(params ClientLogPacket[] packet) {  }
+        private Loader(string s) {
+            PluginConfig = new PluginConfig(s);
+            PluginConfigDirectory = Path.GetDirectoryName(s);
+        }
+
+        public override PluginConfig PluginConfig { get; }
+        public override string PluginConfigDirectory { get; }
+
+        public override void Connect(int id, string image) { }
+
+        public override void DoLog(params ClientLogPacket[] packet) { }
 
         public override void DoInject(int id) {
             _pids.Add(id);
@@ -24,8 +32,6 @@
             loader.Run();
         }
 
-        private Loader(string s) { PluginConfigPath = s; }
-
         private void Run() {
             try {
                 var args = GetRest(Environment.CommandLine, 2);
@@ -37,7 +43,7 @@
                     var process = Process.GetProcessById(pid);
                     process.WaitForExit();
                 }
-            } catch(Exception ex){
+            } catch (Exception ex) {
                 Console.WriteLine(ex);
                 // ignored
             }
